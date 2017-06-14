cfee264c8bd34a63a845f1108b0f41b8829a1e97
Seungwoo Lee <outtoin@kaist.ac.kr>
2017-06-14 17:00:27 +0900

    stash

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,31 +1,11 @@
-import * as types from "../actions/ActionTypes";
+import number from "./number";
+import color from "./color";
 
-const initialState = {
-    color: "black",
-    number: 0
-};
+import { combineReducers } from "redux";
 
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case types.INCREMENT:
-            return {
-                ...state,
-                number: state.number + 1
-            };
-        case types.DECREMENT:
-            return {
-                ...state,
-                number: state.number - 1
-            };
-        case types.SET_COLOR:
-            return {
-                ...state,
-                color: action.color
-            };
+const reducers = combineReducers({
+    numberData: number,
+    colorData: color
+});
 
-        default:
-            return state;
-    }
-}
-
-export default counter;
+export default reducers;
